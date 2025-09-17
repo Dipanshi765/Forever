cf2d65a98a9688ff6e54ca51db3f6ececf1714b9
Dipanshi <dipanshi.diwakar765@gmail.com>
2025-09-17 22:50:44 +0530

    correction

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,13 +1,10 @@
 import React from 'react'
-import {assets} from '../assets/assets'
-import tailwindcss  from '@tailwindcss/vite'
+import { assets } from '../assets/assets'
 
 const Navbar = () => {
   return (
-    <div className='flex items-center justify-between py-5 font-medium'>
-
-        <img src="assets.logo" className='w-36' alt="" />
-      
+    <div className="flex items-center justify-between py-5 font-medium">
+      <img src={assets.logo} className="w-36" alt="Logo" />
     </div>
   )
 }
